e7eb20cbaceb32eaf503edaa58f5f0528a553a01
dennis <dennis.olimpio0794@gmail.com>
2020-09-04 16:42:17 +0200

    0.6
    Ho creato il model Car.php  + add  $fillable

---
+++ laravel-base-crud/app/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+  protected $fillable [
+
+    'manufacturer',
+    'Model_Year',
+    'description',
+    'Model',
+    'transmission',
+    'fuel_type',
+    'Doors'
+    
+  ];
+
+
+
+}
